200ef408f2262f424b4b32c734b3779aac3a3bcd
Maxim Samokhin <scaryck@gmail.com>
2021-12-21 10:31:23 +0300

    change rebase.sh

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -1,13 +1,9 @@
-#1 /bin/bash
+#!/bin/bash
 # display command line options
 
 count=1
 for param in "$@"; do
-<<<<<<< HEAD
-    echo "\$@ Parameter #$count = $param"
-=======
     echo "Parameter: $param"
->>>>>>> 40cab95 (git-rebase 1)
     count=$(( $count + 1 ))
 done
 
